f35cb88c0b6c9dd1ec1491e3ab5244a6897ca39c
Kleric93 <121462973+Kleric93@users.noreply.github.com>
2023-02-08 13:10:13 +0100

    Lecture 9 Accessing the Online Subsystem
    https://www.udemy.com/course/unreal-engine-5-cpp-multiplayer-shooter/learn/lecture/31455746#questions

--- Source/MenuSystem/MenuSystemCharacter.h
+++ Source/MenuSystem/MenuSystemCharacter.h
@@ -62,5 +62,11 @@ public:
 	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
 	/** Returns FollowCamera subobject **/
 	FORCEINLINE class UCameraComponent* GetFollowCamera() const { return FollowCamera; }
+
+public:
+
+	// pointer to the online session interface
+	TSharedPtr<class IOnlineSession, ESPMode::ThreadSafe> OnlineSessionInterface;
+
 };
 
